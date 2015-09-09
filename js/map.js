a4109c4cf816d0c2f095884696ad6460c343444d
NilayKulkarni <typhoscientic@gmail.com>
2015-09-09 13:45:39 +0530

    Sped up updating

---
+++ js/map.js
@@ -0,0 +1,43 @@
+function initMap() {
+	//Main Map
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 20.0000, lng: 73.7800},
+		zoom: 13
+	});
+	//Rokdoba
+	var rokdobaLatLong = {lat:20.00294, lng:73.79324};
+	var rokdobaTitle = "Rokdoba Exit";
+	
+	var rokdobaMarker = new google.maps.Marker({
+		position : rokdobaLatLong,
+		animation: google.maps.Animation.DROP,
+		title : rokdobaTitle,
+		map : map
+	});
+	
+	var rokdobaInfoWindow = new google.maps.InfoWindow({
+		content : rokdobaTitle
+	});
+	
+	rokdobaMarker.addListener('click', function(){
+		rokdobaInfoWindow.open(map, rokdobaMarker);
+	});
+	//Amardham
+	var amardhamLatLong = {lat:19.99866, lng:73.79690};
+	var amardhamTitle = "Amardham Exit";
+	
+	var amardhamMarker = new google.maps.Marker({
+		position : amardhamLatLong,
+		animation: google.maps.Animation.DROP,
+		title : amardhamTitle,
+		map : map
+	});
+	
+	var amardhamInfoWindow = new google.maps.InfoWindow({
+		content : amardhamTitle
+		});
+	
+	amardhamMarker.addListener('click', function(){
+		amardhamInfoWindow.open(map, amardhamMarker);
+	});
+}
