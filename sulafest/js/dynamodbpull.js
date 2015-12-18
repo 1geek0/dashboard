00e15ddcdc61ca7b8737295fa9ee697d91e43b14
NilayKulkarni <typhoscientic@gmail.com>
2015-12-18 16:22:18 +0530

    Changed naming convention. Commented out old scripts

--- sulafest/js/dynamodbpull.js
+++ sulafest/js/dynamodbpull.js
@@ -44,12 +44,7 @@ var countGharpure = document.getElementById('countGharpure');
 var countPanchavati = document.getElementById('countPanchavati');
 var megaCountTextView = document.getElementById('megaCountText');
 
-//DynamoDB initialization
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-	IdentityPoolId: 'us-east-1:08e41de7-9cb0-40d6-9f04-6f8956ed25bb'
-});
-AWS.config.region = 'us-east-1';
-var db = new AWS.DynamoDB();
+
 
 //Utility Variables
 var megaCount = 0; //Int for the megacount
